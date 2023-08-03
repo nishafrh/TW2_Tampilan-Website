cebbf459b8996faa8ec0d3719cd24a6779380420
Nisha Farah Fadilla <nishafarah24.2@gmail.com>
2023-08-03 13:34:09 +0700

    Finish

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html", "./build/js/*.js"],
+  // content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {
+      // colors: {
+      //   leaf: {
+      //     light: '#fef4e3',
+      //     default: '#ff3fd5',
+      //     dark: '#fee5bc',
+      //   }
+      // },
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)'}, 
+        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)'},
+      },
+      keyframes: {
+        'open-menu': { 
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.01)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-debug-screens'),
+  ],
+}
